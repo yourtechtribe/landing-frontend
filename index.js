0ec841a0cd28c3b43d604e8c0e04e5faaa40602a
Jordi Salazar Badia <jordisalazarbadia@gmail.com>
2025-01-25 16:58:50 +0100

    sonar fail

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // Route: GET /
-app.get('/', (req, res) =>      {
-  
-  
-  
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 // Start server
-         if (require.main === module) {
+if (require.main == module) {
   app.listen(port, () => {
-    
-    
-    
-    console.log(`Server is running at http://localhost:${port}`)
+    console.log(`Server is running at http://localhost:${port}`);
   });
-
-
-
 }
 
-module.exports = app // Export app for testing
+module.exports = app; // Export app for testing
